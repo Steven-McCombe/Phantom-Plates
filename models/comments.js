9a1c716640f0c48e6387bf913ebbabe1679f73cb
Steven-McCombe <smccombe93@gmail.com>
2023-01-09 22:41:47 -0500

    added comments model

---
+++ models/comments.js
@@ -0,0 +1,50 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class Comments extends Model {}
+
+Comments.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    rating: {
+        type: DataTypes.INTEGER,
+        
+    },
+    comment_body: {
+      type: DataTypes.TEXT,
+    },
+    created_at: {
+      type: DataTypes.DATE,
+      allowNull: false,
+      defaultValue: DataTypes.NOW,
+    },
+    kitchen_id: {
+    type: DataTypes.INTEGER,
+    references: {
+        model: 'kitchen',
+        key: 'id'
+    }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: 'user',
+        key: 'id',
+      },
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'comments',
+  }
+);
+
+module.exports = Comments;

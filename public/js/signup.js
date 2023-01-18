5797f1789123f8e02dfa548f593a2dcebdafb41a
Steven-McCombe <smccombe93@gmail.com>
2023-01-17 20:09:41 -0500

    added address form

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,9 +4,6 @@ const signupFormHandler = async (event) => {
     const name = document.querySelector('#name-signup').value.trim();
     const email = document.querySelector('#email-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
-  console.log(email)
-  console.log(password)
-  console.log(name)
 
     if (name && email && password) {
       const response = await fetch('/api/user', {
